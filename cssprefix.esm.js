53286f2b4fd3ffc19cf0baf85bbc75268a557a70
anseki <AnSeki.aff@gmail.com>
2018-03-01 17:00:03 +0900

    Change build script

--- cssprefix.esm.js
+++ cssprefix.esm.js
@@ -1,75 +1,7 @@
-var CSSPrefix =
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, {
-/******/ 				configurable: false,
-/******/ 				enumerable: true,
-/******/ 				get: getter
-/******/ 			});
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/* ================================================
+        DON'T MANUALLY EDIT THIS FILE
+================================================ */
 
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /*
  * CSSPrefix
  * https://github.com/anseki/cssprefix
@@ -161,16 +93,10 @@ cssSupports = function () {
     propNames = {},
     propValues = {}; // Cache
 
-// [DEBUG]
-window.normalizeName = normalizeName;
-window.normalizeValue = normalizeValue;
-window.cssSupports = cssSupports;
-// [/DEBUG]
 
 function getName(propName) {
   propName = normalizeName(propName);
   if (propName && propNames[propName] == null) {
-    window.getNameDone = 'get'; // [DEBUG/]
     var declaration = getDeclaration();
 
     if (declaration[propName] != null) {
@@ -204,7 +130,6 @@ function getValue(propName, propValue) {
   propValues[propName] = propValues[propName] || {};
   (Array.isArray(propValue) ? propValue : [propValue]).some(function (propValue) {
     propValue = normalizeValue(propValue);
-    (window.getValueDone = window.getValueDone || []).push(propValue); // [DEBUG/]
 
     if (propValues[propName][propValue] != null) {
       // Cache
@@ -215,7 +140,6 @@ function getValue(propName, propValue) {
         return false; // Continue to next value
       }
     }
-    window.getValueDone.push('get'); // [DEBUG/]
 
     if (cssSupports(propName, propValue)) {
       // Original
@@ -247,8 +171,4 @@ var CSSPrefix = {
   getValue: getValue
 };
 
-/* harmony default export */ __webpack_exports__["default"] = (CSSPrefix);
-
-/***/ })
-/******/ ])["default"];
-//# sourceMappingURL=cssprefix.js.map
+export default CSSPrefix;
