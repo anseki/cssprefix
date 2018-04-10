ba8da85838f5b4862a5818670303144e34c6f966
anseki <AnSeki.aff@gmail.com>
2018-04-10 15:27:40 +0900

    Fix: nested `node_module` is not checked

--- test/httpd.js
+++ test/httpd.js
@@ -6,50 +6,56 @@ const
   DOC_ROOT = __dirname,
   PORT = 8080,
 
+  MODULE_PACKAGES = [
+    'jasmine-core',
+    'test-page-loader'
+  ],
+
   http = require('http'),
   staticAlias = require('node-static-alias'),
-  log4js = require('log4js');
-
-log4js.configure({
-  appenders: {
-    out: {
-      type: 'console',
-      layout: {
-        type: 'pattern',
-        pattern: '%[[%r]%] %m' // Super simple format
-      }
-    }
-  },
-  categories: {default: {appenders: ['out'], level: 'info'}}
-});
-let logger = log4js.getLogger('node-static-alias');
+  logger = (() => {
+    const log4js = require('log4js');
+    log4js.configure({
+      appenders: {
+        out: {
+          type: 'console',
+          layout: {
+            type: 'pattern',
+            pattern: '%[[%r]%] %m' // Super simple format
+          }
+        }
+      },
+      categories: {default: {appenders: ['out'], level: 'info'}}
+    });
+    return log4js.getLogger('node-static-alias');
+  })();
 
 http.createServer((request, response) => {
   request.addListener('end', () => {
     (new staticAlias.Server(DOC_ROOT, {
       cache: false,
       headers: {'Cache-Control': 'no-cache, must-revalidate'},
-      alias: [
-        // node_modules
-        {
-          match: /^\/(?:jasmine-core|test-page-loader)\/.+/,
-          serve: '../node_modules<% reqPath %>',
+      alias: MODULE_PACKAGES.map(packageName => (
+        { // node_modules
+          match: new RegExp(`^/${packageName}/.+`),
+          serve: `${require.resolve(packageName).replace(
+            // Include `packageName` for nested `node_modules`
+            new RegExp(`^(.*[/\\\\]node_modules)[/\\\\]${packageName}[/\\\\].*$`), '$1')}<% reqPath %>`,
           allowOutside: true
-        }
-      ],
-      logger: logger
+        })),
+      logger
     }))
-    .serve(request, response, e => {
-      if (e) {
-        response.writeHead(e.status, e.headers);
-        logger.error('(%s) %s', request.url, response.statusCode);
-        if (e.status === 404) {
-          response.end('Not Found');
+      .serve(request, response, e => {
+        if (e) {
+          response.writeHead(e.status, e.headers);
+          logger.error('(%s) %s', request.url, response.statusCode);
+          if (e.status === 404) {
+            response.end('Not Found');
+          }
+        } else {
+          logger.info('(%s) %s', request.url, response.statusCode);
         }
-      } else {
-        logger.info('(%s) %s', request.url, response.statusCode);
-      }
-    });
+      });
   }).resume();
 }).listen(PORT);
 
